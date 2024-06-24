ae8091b504fb0f54ce4a650d720ea21ccc593e23
Anand Kumar Gautam <anandvns0510@gmail.com>
2024-06-24 23:39:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react'
+import Header from './components/header/Header';
+
+const App = () => {
+  return (
+    <div>
+      <header/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../header/header.css'
+
+const Header = () => {
+  return (
+    <div>header</div>
+  )
+}
+
+export default Header
